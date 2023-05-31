5d56befef4514718efae3935734842d75820d6e8
xeylou <xeylou.pro@gmail.com>
2023-05-31 17:30:36 +0200

    removing unused files

--- php/unused/merde_ctg.php
+++
@@ -1,8 +0,0 @@
-<?php
-                // SI ON AJOUTE DES CATEGORIES LE CODE SUIVRA & AJOUTERA
-                $req_ctg = 'SELECT * FROM categories';
-                $res_ctg = $db->query($req_ctg);
-                while ($data=$res_ctg->fetchArray()) {
-                    echo '';
-                }
-                ?>
